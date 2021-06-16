f68f3883adf213f80efa238ebc4a56a5e478aa92
Bruce Rick <brucericktgs@gmail.com>
2021-06-15 21:33:08 -0400

    Initial commit

---
+++ PKDB/PKDB/MainView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  Pokedex
+//
+//  Created by Bruce Rick on 2021-03-21.
+//
+
+import SwiftUI
+
+struct MainView: View {
+  var body: some View {
+    NavigationView {
+      List {
+        cell(text: "Games", iconName: "gamecontroller.fill", destination: EmptyView())
+        cell(text: "Pokedexes", iconName: "book.fill", destination: EmptyView())
+      }
+      .navigationBarTitle(Text("Pokemon Database"))
+    }
+  }
+}
+
+func cell<Destination: View>(text: String, iconName: String, destination: Destination) -> some View {
+  NavigationLink(destination: destination.navigationTitle(text)) {
+    HStack {
+      Image(systemName: iconName)
+        .font(.system(size: 22, weight: .regular))
+        .frame(width: 30, height: 22)
+        .padding(.horizontal, 5)
+      Text(text)
+      Spacer()
+    }.frame(minHeight: 44)
+  }
+}
+
+struct MainView_Previews: PreviewProvider {
+  static var previews: some View {
+    MainView()
+  }
+}
---
+++ PKDB/PKDB/PKDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  PKDBApp.swift
+//  PKDB
+//
+//  Created by Bruce Rick on 2021-06-15.
+//
+
+import SwiftUI
+
+@main
+struct PKDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
